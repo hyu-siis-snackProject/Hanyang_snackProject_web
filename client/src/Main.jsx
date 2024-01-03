a33b29e1099b3d79b43e12fe0c0948f680aa556b
ragnar <mystparkkorea@gmail.com>
2024-01-04 00:42:26 +0900

    DrawingPaper 제작

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,10 +1,13 @@
 import 'src/main.css';
 import TempBox from 'components/TempBox';
+import DrawingPaper from './pages/DrawingPaper';
 
 const Main = () => (
   <div className="mainLayout">
     <TempBox />
     <div>프로젝트 초기 세팅</div>
+    {/* UI 컴포넌트 제작시 사용할 임시 페이지  */}
+    <DrawingPaper />
   </div>
 );
 export default Main;
---
+++ client/src/pages/DrawingPaper/index.jsx
@@ -0,0 +1,5 @@
+// temp Page
+// UI 제작 테스트를 위한 페이지
+const DrawingPaper = () => <div>Drawing Paper</div>;
+
+export default DrawingPaper;
